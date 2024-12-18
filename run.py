97efdf3f95a3c43e9cdf012d006519d3cca5233a
pbusenius <patrick.busenius@gmail.com>
2024-12-18 17:50:49 +0100

    added lat lon example

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import polars as pl
-from polars_maxminddb import ip_lookup_city, ip_lookup_country, ip_lookup_asn
+from polars_maxminddb import ip_lookup_city, ip_lookup_country, ip_lookup_asn, ip_lookup_latitude, ip_lookup_longitude
 
 # Example DataFrame with IP addresses
 df = pl.DataFrame(
@@ -27,6 +27,11 @@ df = df.with_columns(
 # Perform ASN lookup
 df = df.with_columns(
     ip_lookup_asn(df["ip_addresses"], "GeoLite2-ASN.mmdb").alias("asn_name")
+)
+
+df = df.with_columns(
+    ip_lookup_latitude(df["ip_addresses"], "GeoLite2-City.mmdb").alias("latitude"),
+    ip_lookup_longitude(df["ip_addresses"], "GeoLite2-City.mmdb").alias("longitude")
 )
 
 print(df)
